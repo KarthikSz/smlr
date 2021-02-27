cf4836707ea2f04d4ab69bceffb87cb5ecb74887
BharathKumarRavichandran <bharathkumarravichandran@gmail.com>
2021-02-28 04:21:16 +0530

    Add models for Vidoes, Summary, Questions

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Videos(models.Model):
+    video_id = models.AutoField(primary_key=True)
+    title = models.TextField(max_length=2000, null=True)
+    filename = models.TextField(max_length=2000, unique=True, null=False)
+    is_processed = models.BooleanField(default=False)
+
+
+class Summary(models.Model):
+    summary_id = models.AutoField(primary_key=True)
+    video_id = models.ForeignKey(Videos, db_column='video_id', on_delete=models.CASCADE)
+    text = models.TextField(max_length=20000, null=True)
+
+
+class Questions(models.Model):
+    question_id = models.AutoField(primary_key=True)
+    video_id = models.ForeignKey(Videos, db_column='video_id', on_delete=models.CASCADE)
+    question = models.TextField(max_length=2000, null=False)
